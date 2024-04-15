2d5fff9d91e57b8cd7f80fd0cbc4eda5d60b0360
IvanBermejoHidalgo <ibermejo@elpuig.xeill.net>
2024-04-15 17:51:04 +0200

    guardar nombre de usuario

--- app/src/main/java/com/example/socialpuig/RegisterFragment.java
+++ app/src/main/java/com/example/socialpuig/RegisterFragment.java
@@ -1,5 +1,6 @@
 package com.example.socialpuig;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -14,6 +15,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -21,13 +24,15 @@ import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 
 public class RegisterFragment extends Fragment {
 
     NavController navController;
-    private EditText emailEditText, passwordEditText;
+    private EditText usernametxt,emailEditText, passwordEditText;
     private Button emailSignInButton2;
+    TextView displayNameTextView;
     private FirebaseAuth mAuth;
 
     @Override
@@ -74,25 +79,25 @@ public class RegisterFragment extends Fragment {
         navController = Navigation.findNavController(view);
 
         mAuth = FirebaseAuth.getInstance();
-
+        usernametxt = view.findViewById(R.id.usernametxt);
         emailEditText = view.findViewById(R.id.emailEditTextreg);
         passwordEditText = view.findViewById(R.id.passwordEditText);
+        displayNameTextView = view.findViewById(R.id.displayNameTextView);
 
         emailSignInButton2 = view.findViewById(R.id.emailSignInButton2);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
 
         emailSignInButton2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                crearCuenta();
+                crearCuenta(user);
             }
         });
 
     }
-    private void crearCuenta() {
-        //if (!validarFormulario()) {
-        //  return;
-        //}
-
+    private void crearCuenta(FirebaseUser user) {
         emailSignInButton2.setEnabled(false);
 
         mAuth.createUserWithEmailAndPassword(emailEditText.getText().toString(), passwordEditText.getText().toString())
@@ -100,22 +105,53 @@ public class RegisterFragment extends Fragment {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            actualizarUI(mAuth.getCurrentUser());
+                            FirebaseUser currentUser = mAuth.getCurrentUser();
+                            // Guardar el nombre de usuario en la base de datos
+                            guardarNombreUsuario(currentUser, usernametxt.getText().toString());
+                            // Actualizar la interfaz de usuario
+                            actualizarUI(currentUser);
                         } else {
                             Snackbar.make(requireView(), "Error: " + task.getException(), Snackbar.LENGTH_LONG).show();
-
+                            emailSignInButton2.setEnabled(true);
                         }
-                        emailSignInButton2.setEnabled(true);
                     }
                 });
+    }
 
+    private void guardarNombreUsuario(FirebaseUser user, String username) {
+        String newName = String.valueOf(usernametxt.getText());
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setDisplayName(username)
+                .build();
+
+        user.updateProfile(profileUpdates)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            // El nombre de usuario se actualizó correctamente
+                            //displayNameTextView.setText(newName);
+                            MainActivity mainActivity = (MainActivity) getActivity(); // Obtén una referencia a MainActivity
+                            if (mainActivity != null) {
+                                mainActivity.updateNavigationHeaderName(newName); // Llama al método de actualización en MainActivity
+                            }
+                        } else {
+                            // Error al actualizar el nombre de usuario
+                        }
+                    }
+                });
     }
 
     private void actualizarUI(FirebaseUser currentUser) {
         if(currentUser != null){
+            String username = currentUser.getDisplayName();
+            if (!TextUtils.isEmpty(username)) {
+                Toast.makeText(requireContext(), "Bienvenido, " + username, Toast.LENGTH_SHORT).show();
+            }
             navController.navigate(R.id.homeFragment);
         }
     }
+
 /*
     private boolean validarFormulario() {
         boolean valid = true;
