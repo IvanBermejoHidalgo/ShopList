b8eae7185bc4ebfbc82ab982aa468fd8acefb8ed
Iván <ibermejo@elpuig.xeill.net>
2024-05-01 16:37:51 +0200

    se guardan en firebase

--- app/src/main/java/com/example/socialpuig/Activity/NuevaListaActivity.java
+++ app/src/main/java/com/example/socialpuig/Activity/NuevaListaActivity.java
@@ -1,6 +1,7 @@
 package com.example.socialpuig.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
 
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +10,8 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.socialpuig.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -46,17 +49,26 @@ public class NuevaListaActivity extends AppCompatActivity {
             FirebaseUser user = mAuth.getCurrentUser();
             if (user != null) {
                 String userId = user.getUid();
-                String listaId = mDatabase.child("Users").child(userId).child("Listas").push().getKey();
-                if (listaId != null) {
-                    // Guardar la nueva lista en la base de datos
-                    mDatabase.child("Users").child(userId).child("Listas").child(listaId).child("nombre").setValue(nombreLista);
-
-                    Toast.makeText(NuevaListaActivity.this, "Nueva lista creada con éxito", Toast.LENGTH_SHORT).show();
-                    finish(); // Finalizar la actividad después de guardar la lista
-                }
+                String listaId = nombreLista; // Usar el nombre de la lista como el ID
+                // Guardar la nueva lista en la base de datos
+                mDatabase.child("Users").child(userId).child("Listas").child(listaId).child("nombre").setValue(nombreLista)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(NuevaListaActivity.this, "Nueva lista creada con éxito", Toast.LENGTH_SHORT).show();
+                                finish(); // Finalizar la actividad después de guardar la lista
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(NuevaListaActivity.this, "Error al guardar la lista: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
             }
         } else {
             Toast.makeText(NuevaListaActivity.this, "Por favor, introduce un nombre para la lista", Toast.LENGTH_SHORT).show();
         }
     }
+
 }
